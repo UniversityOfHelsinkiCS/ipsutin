744691afc483edfbbfdc03286135ce385788dae7
Henri Remonen <henri@remonen.fi>
2023-04-03 14:16:10 +0300

    init ipsutin and delete kliksutin stuff

--- src/client/hooks/usePersistForm.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react'
-import { PersistForm } from '../types'
-
-const usePersistForm = ({ value, sessionStorageKey }: PersistForm) =>
-  useEffect(() => {
-    sessionStorage.setItem(sessionStorageKey, JSON.stringify(value))
-  }, [value, sessionStorageKey])
-
-export default usePersistForm
--- src/client/react-app-env.d.ts
+++
@@ -1,2 +0,0 @@
-// this file is actually important, for things like svg to work in ts
-/// <reference types="react-scripts" />
