3bf1b8945e5bba849e6324e0dc48881f594bb74c
Henri Remonen <henri@remonen.fi>
2023-04-12 10:06:49 +0300

    Change process.env to Vite import.meta.env: https://vitejs.dev/guide/env-and-mode.html

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,17 @@
-export const inDevelopment = process.env.NODE_ENV === 'development'
+export const inDevelopment = import.meta.env.NODE_ENV === 'development'
 
-export const inStaging = process.env.REACT_APP_STAGING === 'true'
+export const inStaging = import.meta.env.REACT_APP_STAGING === 'true'
 
-export const inProduction = !inStaging && process.env.NODE_ENV === 'production'
+export const inProduction =
+  !inStaging && import.meta.env.NODE_ENV === 'production'
 
-export const inE2EMode = process.env.REACT_APP_E2E === 'true'
+export const inE2EMode = import.meta.env.REACT_APP_E2E === 'true'
 
-export const GIT_SHA = process.env.REACT_APP_GIT_SHA || ''
+export const GIT_SHA = import.meta.env.REACT_APP_GIT_SHA || ''
 
-export const PUBLIC_URL = process.env.PUBLIC_URL || ''
+export const PUBLIC_URL = import.meta.env.PUBLIC_URL || ''
 
 export const DEFAULT_SURVEY_NAME =
-  process.env.DEFAULT_SURVEY_NAME || 'testSurvey'
+  import.meta.env.DEFAULT_SURVEY_NAME || 'testSurvey'
 
 export const FORM_DATA_KEY = 'curre_local_save'
