647ee34ac4a7f9147d6bd7081a091a2d2c3d08cd
erikao1998 <ersku1998@gmail.com>
2023-04-20 14:00:32 +0300

    Switch links to react router links

--- src/client/components/MainPage.tsx
+++ src/client/components/MainPage.tsx
@@ -1,24 +1,19 @@
 import React from 'react'
 
-import { Box, Link, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 const MainPage = () => (
   <Typography variant="h6">
     <h2>Which of these surveys would you like to use?</h2>
     <Box sx={{ minHeight: 50 }}>
-      <Link href="/ipassessment" underline="hover">
-        IP Assessment
-      </Link>
+      <Link to="/ipassessment">IP Assessment</Link>
     </Box>
     <Box sx={{ minHeight: 50 }}>
-      <Link href="/licences" underline="hover">
-        Licences
-      </Link>
+      <Link to="/licences">Licences</Link>
     </Box>
     <Box sx={{ minHeight: 50 }}>
-      <Link href="/ideaevaluation" underline="hover">
-        Idea Evaluation
-      </Link>
+      <Link to="/ideaevaluation">Idea Evaluation</Link>
     </Box>
   </Typography>
 )
