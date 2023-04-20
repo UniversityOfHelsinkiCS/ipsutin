327a5a253d51569b68070677d1d94cf48a668ec5
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-04-20 13:02:50 +0300

    Update prod base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { inStaging } from './src/config'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: inStaging ? '/ipsutin' : '/',
+  base: inStaging ? '/ipsutin' : '',
   server: {
     proxy: {
       '/api/': {
