59114d1286066df9911f393134569e2af90dcc7a
erikao1998 <ersku1998@gmail.com>
2023-05-10 15:45:22 +0300

    get texts from locales folder

--- src/client/components/MainPage/HelloBanner.tsx
+++ src/client/components/MainPage/HelloBanner.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Box } from '@mui/material'
+import { useTranslation } from 'react-i18next'
 import styles from '../../styles'
 
 const HelloBanner = () => {
+  const { t } = useTranslation()
   const { cardStyles } = styles
 
   return (
     <Box id="hello-component" sx={cardStyles.helloBox}>
       <Box>
-        <h2>Welcome to Ipsutin</h2>
+        <h2>{t('helloBanner:welcomeMessage')}</h2>
         <div style={{ fontSize: '1.1rem' }}>Placeholder text</div>
       </Box>
     </Box>
--- src/client/components/MainPage/MainPage.tsx
+++ src/client/components/MainPage/MainPage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 import { Box, Grid } from '@mui/material'
 import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 import SelectFaculty from './SelectFaculty'
 import HelloBanner from './HelloBanner'
 import styles from '../../styles'
@@ -10,6 +11,7 @@ import ShowMore from '../Common/ShowMore'
 const MainPage = () => {
   const { formStyles } = styles
   const [faculty, setFaculty] = useState('')
+  const { t } = useTranslation()
 
   return (
     <Box sx={formStyles.formWrapper}>
@@ -27,17 +29,17 @@ const MainPage = () => {
           <SelectFaculty setFaculty={setFaculty} faculty={faculty} />
         </Grid>
         <Grid item sm={12} md={5} xl={6}>
-          <h2>Which of these surveys would you like to use?</h2>
+          <h2>{t('mainPage:toolSelectionQuestion')}</h2>
           <Box sx={{ minHeight: 50 }}>
-            <Link to="/ipassessment">IP Assessment</Link>
+            <Link to="/ipassessment">{t('surveyNames:ipAssessment')}</Link>
             <ShowMore text="placeholder description" />
           </Box>
           <Box sx={{ minHeight: 50 }}>
-            <Link to="/licences">Licences</Link>
+            <Link to="/licences">{t('surveyNames:licences')}</Link>
             <ShowMore text="placeholder description" />
           </Box>
           <Box sx={{ minHeight: 50 }}>
-            <Link to="/ideaevaluation">Idea Evaluation</Link>
+            <Link to="/ideaevaluation">{t('surveyNames:ideaEvaluation')}</Link>
             <ShowMore text="placeholder description" />
           </Box>
         </Grid>
