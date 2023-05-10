ba45d56e3ccc70024d914ff4ab6953ff40ce3802
erikao1998 <ersku1998@gmail.com>
2023-05-10 15:05:59 +0300

    add placeholder descriptions for surveys in main page

--- src/client/components/MainPage/MainPage.tsx
+++ src/client/components/MainPage/MainPage.tsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 import SelectFaculty from './SelectFaculty'
 import HelloBanner from './HelloBanner'
 import styles from '../../styles'
+import ShowMore from '../Common/ShowMore'
 
 const MainPage = () => {
   const { formStyles } = styles
@@ -29,12 +30,15 @@ const MainPage = () => {
           <h2>Which of these surveys would you like to use?</h2>
           <Box sx={{ minHeight: 50 }}>
             <Link to="/ipassessment">IP Assessment</Link>
+            <ShowMore text="placeholder description" />
           </Box>
           <Box sx={{ minHeight: 50 }}>
             <Link to="/licences">Licences</Link>
+            <ShowMore text="placeholder description" />
           </Box>
           <Box sx={{ minHeight: 50 }}>
             <Link to="/ideaevaluation">Idea Evaluation</Link>
+            <ShowMore text="placeholder description" />
           </Box>
         </Grid>
       </Grid>
