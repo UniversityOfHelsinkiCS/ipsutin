0c15a8e339c91a21cd14a178cfed13b64bdc36f4
erikao1998 <ersku1998@gmail.com>
2023-05-17 09:40:27 +0300

    Add styles and placeholder content to result box

--- src/client/components/Results/Results.tsx
+++ src/client/components/Results/Results.tsx
@@ -9,13 +9,34 @@ import styles from '../../styles'
 const Results = ({ watch }: InputProps) => {
   const { t } = useTranslation()
   const [openContactForm, setOpenContactForm] = useState(false)
-  const { formStyles } = styles
+  const { formStyles, cardStyles } = styles
 
   console.log(watch)
 
   return (
-    <Box>
-      <Box>Results</Box>
+    <Box sx={cardStyles.outerBox}>
+      <Box>
+        <h2>{t('results:title')}</h2>
+      </Box>
+      <Box>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae sem
+        dolor. Nulla pulvinar volutpat mauris, lobortis porttitor risus placerat
+        id. Nunc eu tempus eros. Etiam placerat dignissim tellus a commodo. In
+        in tristique dolor. Maecenas vulputate tempor tortor sed consectetur.
+        Nam sagittis sit amet ex pulvinar sodales. Nullam vulputate sapien
+        nulla, a malesuada ligula posuere id. Nam justo ligula, accumsan quis
+        nisl nec, maximus congue est. Aenean imperdiet justo ac lorem porttitor
+        consectetur.{' '}
+      </Box>
+      <Box>
+        Nullam gravida nulla arcu, et auctor massa sollicitudin a. Aliquam
+        aliquam nisi odio, sit amet interdum tellus tincidunt eu. Donec et dolor
+        egestas, gravida magna a, placerat leo. Pellentesque commodo diam ac
+        libero malesuada efficitur. Proin sed nisl quis eros aliquet aliquam.
+        Nam nec turpis mattis, lobortis ligula eget, maximus justo. Maecenas
+        enim nulla, venenatis vitae odio quis, faucibus congue est. Duis quis
+        sem nec mauris sagittis condimentum.
+      </Box>
       <Box sx={formStyles.stackBoxWrapper}>
         <Stack sx={formStyles.stack} direction="row">
           <SendSummaryEmail />
