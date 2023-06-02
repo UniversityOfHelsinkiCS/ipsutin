02026839eb28fe035d93392dab3009d4f88e32a6
Henri Remonen <henri@remonen.fi>
2023-06-02 11:19:05 +0300

    Seed the license results to db

--- src/server/db/seeders/results.ts
+++ src/server/db/seeders/results.ts
@@ -1,12 +1,14 @@
 import { Result } from '../models'
 import getIeResults from '../../data/ieResults'
 import getAssessmentResults from '../../data/assessmentResults'
+import getLicenseResults from '../../data/licenseResults'
 
 const seedResults = async () => {
-  const ieResults: any[] = getIeResults()
-  const assessmentResults: any[] = getAssessmentResults()
+  const ieResults = getIeResults()
+  const assessmentResults = getAssessmentResults()
+  const licenseResults = getLicenseResults()
 
-  const results = [...ieResults, ...assessmentResults]
+  const results = [...ieResults, ...assessmentResults, ...licenseResults]
 
   results.forEach(async (result: any) => {
     await Result.upsert({
