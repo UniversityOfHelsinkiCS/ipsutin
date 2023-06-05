85b610f5cbcf1c0b81eac63be9562a4a60ad3ef2
Henri Remonen <henri@remonen.fi>
2023-06-05 10:41:02 +0300

    Coloring changes

--- src/client/util/colors.ts
+++ src/client/util/colors.ts
@@ -1,11 +1,11 @@
 const colors: { [key: string]: string } = {
-  his: '#f18235',
+  his: '#ed1975',
   clinic: '#8261a1',
   corporate: '#23439b',
   incubator: '#199995',
   legal: '#afd255',
   gnu_gpl: '#eb4034',
-  bsd_mit: '#ed1975',
+  bsd_mit: '#f18235',
 }
 
 export default colors
