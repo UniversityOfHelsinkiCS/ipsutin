245ed0129be49b24eb02327532d478ffdca017f5
Henri Remonen <henri@remonen.fi>
2023-06-07 11:47:33 +0300

    Use sentry error capture only in prod

--- src/server/middeware/error.ts
+++ src/server/middeware/error.ts
@@ -1,11 +1,19 @@
+import { Request, Response, NextFunction } from 'express'
 import Sentry from '@sentry/node'
 
 import logger from '../util/logger'
 
-const errorHandler = (error: any, _req: any, _res: any, next: any) => {
+import { inProduction } from '../../config'
+
+const errorHandler = (
+  error: Error,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   logger.error(`${error.message} ${error.name} ${error.stack}`)
 
-  Sentry.captureException(error)
+  if (inProduction) Sentry.captureException(error)
 
   return next(error)
 }
