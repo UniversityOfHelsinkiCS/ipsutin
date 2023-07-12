7a9b5b668e19407e70160e7fe09d141968d110a3
Henri Remonen <henri@remonen.fi>
2023-07-12 09:42:25 +0300

    Remove unneeded seeding file

--- src/server/db/seeders/results.ts
+++ src/server/db/seeders/results.ts
@@ -1,21 +1,14 @@
 import { Result } from '../models'
-import getAssessmentQuestionaireResults from '../../data/assessmentQuestionaireResults'
 import getAssessmentResults from '../../data/assessmentResults'
 import getIeResults from '../../data/ieResults'
 import getLicenseResults from '../../data/licenseResults'
 
 const seedResults = async () => {
-  const assessmentQuestionnaireResults = getAssessmentQuestionaireResults()
   const assessmentResults = getAssessmentResults()
   const ieResults = getIeResults()
   const licenseResults = getLicenseResults()
 
-  const results = [
-    ...assessmentQuestionnaireResults,
-    ...ieResults,
-    ...assessmentResults,
-    ...licenseResults,
-  ]
+  const results = [...ieResults, ...assessmentResults, ...licenseResults]
 
   results.forEach(async (result) => {
     await Result.upsert({
