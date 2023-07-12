dfb9f13444047ef0634a954de3fd33611b1b3636
Henri Remonen <henri@remonen.fi>
2023-07-12 12:57:16 +0300

    Create seeder file for the ip assessment recommendations

--- src/server/db/seeders/recommendations.ts
+++ src/server/db/seeders/recommendations.ts
@@ -1,12 +1,18 @@
 import { Recommendation } from '../models'
 import getIeRecommendations from '../../data/ieRecommendations'
 import getLicenseRecommendations from '../../data/licenseRecommendations'
+import getAssessmentRecommendations from '../../data/assessmentRecommendations'
 
 const seedRecommendations = async () => {
   const ieRecommendations = getIeRecommendations()
   const licenseRecommendations = getLicenseRecommendations()
+  const assessmentRecommendations = getAssessmentRecommendations()
 
-  const recommendations = [...ieRecommendations, ...licenseRecommendations]
+  const recommendations = [
+    ...assessmentRecommendations,
+    ...ieRecommendations,
+    ...licenseRecommendations,
+  ]
 
   recommendations.forEach(async (recommendation) => {
     await Recommendation.upsert({
