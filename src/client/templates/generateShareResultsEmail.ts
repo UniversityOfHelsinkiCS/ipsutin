45e7b8ca921a134ebb221b508ba1e9a31c868568
Henri Remonen <henri@remonen.fi>
2023-07-17 12:18:31 +0300

    Create email template for sharing results

---
+++ src/client/templates/generateShareResultsEmail.ts
@@ -0,0 +1,34 @@
+import { User } from '../types'
+
+const generateShareResultsEmail = (surveyName: string, user: User) => {
+  const name = {
+    ipassessment: 'IP Assessment',
+    licences: 'Licences',
+    ideaevaluation: 'Idea Evaluation',
+  }
+  return ` \
+<div> \
+  <h3> \
+    <strong> \
+        ${name[surveyName as keyof typeof name]}
+    </strong> \
+  </h3> \
+  <p>
+    Hey
+  </p> \
+  <p>
+    ${user?.firstName} ${
+    user?.lastName
+  } Would like to share their results with you.
+  </p> \
+  <p>
+    ********** 
+    Summary of user's ${user?.firstName} ${user?.lastName} ${
+    name[surveyName as keyof typeof name]
+  } survey selections below
+  </p> \
+</div> \
+`
+}
+
+export default generateShareResultsEmail
