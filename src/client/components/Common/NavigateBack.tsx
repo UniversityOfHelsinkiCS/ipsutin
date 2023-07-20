f23d8a80968dc2f7e26b6d3529e135ad5506c0c1
Henri Remonen <henri@remonen.fi>
2023-07-20 13:13:30 +0300

    Extract navigate back button to own component

---
+++ src/client/components/Common/NavigateBack.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { Button } from '@mui/material'
+
+const NavigateBack = () => {
+  const { t } = useTranslation()
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const onNavigateBack = () => {
+    navigate({
+      pathname: '..',
+      search: location.search,
+    })
+  }
+
+  return (
+    <Button
+      data-cy='back-to-survey-button'
+      sx={{ m: 1 }}
+      onClick={onNavigateBack}
+    >
+      {'<'} {t('results:backToMessage')}
+    </Button>
+  )
+}
+
+export default NavigateBack
