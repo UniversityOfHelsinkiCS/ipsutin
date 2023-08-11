28e0362641c89730f2aee67c2bf698d78aee45ee
Henri Remonen <henri@remonen.fi>
2023-08-11 09:45:34 +0300

    Extract faculty route business logic to service

---
+++ src/server/services/faculty.ts
@@ -0,0 +1,29 @@
+import { inE2EMode } from '../../config'
+import mockFaculty from '../mocs/faculty'
+import { Faculty } from '../types'
+import { getOrganisationData, getUserOrganisations } from '../util/jami'
+
+export const getFaculties = async (): Promise<Faculty[]> => {
+  if (inE2EMode) return mockFaculty
+
+  const organisationData = (await getOrganisationData()) || []
+
+  const faculties = organisationData.map(({ code, name }) => ({ code, name }))
+
+  return faculties
+}
+
+export const getUserFaculties = async (
+  userId: string,
+  iamGroups: string[]
+): Promise<Faculty[]> => {
+  if (inE2EMode) return mockFaculty
+
+  if (!userId) return []
+
+  const organisationData = await getUserOrganisations(userId, iamGroups)
+
+  const faculties = organisationData.map(({ code, name }) => ({ code, name }))
+
+  return faculties
+}
