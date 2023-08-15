65ae3e029863ff0badbf3e16c1343f3ad57a3a22
Henri Remonen <henri@remonen.fi>
2023-08-15 14:31:57 +0300

    Update the shitty entry validation

--- src/validators/entries.ts
+++ src/validators/entries.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const NewEntryZod = z.object({
   sessionToken: z.string().nonempty(),
-  data: z.record(z.string().nonempty()),
+  data: z.record(z.string()),
 })
 
 export type NewEntry = z.infer<typeof NewEntryZod>
