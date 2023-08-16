ce7a7935c86947ee7a1db50e9973354f9d9ed0c0
Henri Remonen <henri@remonen.fi>
2023-08-16 15:12:42 +0300

    Update the LoadingProgress component and use it in the admin panel

--- src/client/components/Admin/Analytics/RenderAnalytics.tsx
+++ src/client/components/Admin/Analytics/RenderAnalytics.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { CircularProgress, Container, Typography } from '@mui/material'
+import { Container, Typography } from '@mui/material'
 
 import { useEntries } from '../../../hooks/useEntry'
 import useFaculties from '../../../hooks/useFaculty'
 import styles from '../../../styles'
+import LoadingProgress from '../../Common/LoadingProgress'
 
 import FacultyAnalytics from './FacultyAnalytics'
 
@@ -16,7 +17,7 @@ const RenderAnalytics = () => {
   const { faculties, isLoading: isFacultiesLoading } = useFaculties()
 
   if (isEntriesLoading || isFacultiesLoading) {
-    return <CircularProgress />
+    return <LoadingProgress />
   }
 
   return (
