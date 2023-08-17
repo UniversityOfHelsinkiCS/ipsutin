af00295029d42ed3163c4d2792aa6bf84f99d791
Henri Remonen <henri@remonen.fi>
2023-08-17 12:52:21 +0300

    Create route for getting the todays user count

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 
 import { User } from '../db/models'
+import adminHandler from '../middleware/admin'
+import { getUserCounts } from '../services/user'
 import { RequestWithUser } from '../types'
 
 const userRouter = express.Router()
@@ -16,6 +18,12 @@ userRouter.get('/login', async (req: RequestWithUser, res) => {
   })
 
   return res.send(updatedUser)
+})
+
+userRouter.get('/userCounts', adminHandler, async (req, res) => {
+  const userCounts = await getUserCounts()
+
+  return res.status(200).send(userCounts)
 })
 
 export default userRouter
---
+++ src/server/services/user.ts
@@ -0,0 +1,37 @@
+import { Op } from 'sequelize'
+
+import { User } from '../db/models'
+
+interface UserCount {
+  name: 'today' | 'deltaAllTime'
+  value: number
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export const getUserCounts = async (): Promise<UserCount[]> => {
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+
+  const countOfAllUsers = await User.count({})
+
+  const countOfTodaysUsers = await User.count({
+    where: {
+      lastLoggedIn: {
+        [Op.gte]: today,
+      },
+    },
+  })
+
+  const userCounts: UserCount[] = [
+    {
+      name: 'today',
+      value: countOfTodaysUsers,
+    },
+    {
+      name: 'deltaAllTime',
+      value: countOfAllUsers - countOfTodaysUsers,
+    },
+  ]
+
+  return userCounts
+}
