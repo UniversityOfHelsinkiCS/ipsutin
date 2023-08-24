46c9350f7673a0c0ab7e33253a33520717cbe88c
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-08-24 12:33:31 +0300

    Update Sentry dsn, release workflow

--- src/client/util/sentry.ts
+++ src/client/util/sentry.ts
@@ -6,7 +6,7 @@ import { GIT_SHA, inProduction, inStaging } from '../../config'
 const initializeSentry = () => {
   if (inProduction || inStaging) {
     Sentry.init({
-      dsn: 'https://506394e42eb84c208fc1dfad42a63425@sentry.cs.helsinki.fi/17',
+      dsn: 'https://f1febb592f09a6756ddfcff7257f45e3@toska.cs.helsinki.fi/10',
       release: GIT_SHA,
       integrations: [new Integrations.BrowserTracing()],
       tracesSampleRate: 1.0,
--- src/server/util/sentry.ts
+++ src/server/util/sentry.ts
@@ -7,7 +7,7 @@ import { GIT_SHA, inProduction, inStaging } from '../../config'
 const initializeSentry = (router: Express) => {
   if (inProduction || inStaging) {
     initSentry({
-      dsn: 'https://506394e42eb84c208fc1dfad42a63425@sentry.cs.helsinki.fi/17',
+      dsn: 'https://f1febb592f09a6756ddfcff7257f45e3@toska.cs.helsinki.fi/10',
       release: GIT_SHA,
       integrations: [
         new Integrations.Http({ tracing: true }),
