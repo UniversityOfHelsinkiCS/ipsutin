7ed78b3781aea4780cddf5fec0aa066486fd7a1f
Henri Remonen <henri@remonen.fi>
2023-09-08 15:42:29 +0300

    refactor: update routes on analytics route

--- src/server/routes/analytic.ts
+++ src/server/routes/analytic.ts
@@ -3,28 +3,43 @@ import express from 'express'
 import adminHandler from '../middleware/admin'
 import {
   getFacultyCounts,
+  getSurveyAnswerDistribution,
   getSurveyCounts,
   getUserCounts,
 } from '../services/analytic'
 
 const analyticRouter = express.Router()
 
-analyticRouter.get('/usercounts', adminHandler, async (req, res) => {
+analyticRouter.get('/counts/user', adminHandler, async (req, res) => {
   const userCounts = await getUserCounts()
 
   return res.status(200).send(userCounts)
 })
 
-analyticRouter.get('/facultycounts', adminHandler, async (req, res) => {
+analyticRouter.get('/counts/faculty', adminHandler, async (req, res) => {
   const facultyCounts = await getFacultyCounts()
 
   return res.status(200).send(facultyCounts)
 })
 
-analyticRouter.get('/surveycounts', adminHandler, async (req, res) => {
+analyticRouter.get('/counts/survey', adminHandler, async (req, res) => {
   const surveyCounts = await getSurveyCounts()
 
   return res.status(200).send(surveyCounts)
 })
+
+analyticRouter.get(
+  '/distribution/:surveyName',
+  adminHandler,
+  async (req, res) => {
+    const { surveyName } = req.params
+
+    const surveyAnswerDistribution = await getSurveyAnswerDistribution(
+      surveyName
+    )
+
+    return res.status(200).send(surveyAnswerDistribution)
+  }
+)
 
 export default analyticRouter
