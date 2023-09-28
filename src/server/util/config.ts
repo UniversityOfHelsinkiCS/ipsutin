5c0026bbb8cdee2dec8fdf1bf343c96430c96547
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-09-28 12:28:45 +0300

    Temporarily use test oidc instance in production

--- src/server/util/config.ts
+++ src/server/util/config.ts
@@ -31,7 +31,7 @@ export const IMPORTER_URL = inProduction
   : 'https://api-toska.apps.ocp-test-0.k8s.it.helsinki.fi/importer/kliksutin'
 
 export const OIDC_ISSUER = inProduction
-  ? 'https://login.helsinki.fi/.well-known/openid-configuration'
+  ? 'https://login-test.it.helsinki.fi/.well-known/openid-configuration'
   : 'https://login-test.it.helsinki.fi/.well-known/openid-configuration'
 
 export const OIDC_CLIENT_ID = process.env.OIDC_CLIENT_ID || ''
