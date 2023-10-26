208d08ad9aeeabc7bdde702b4a8e0f1a578c1bfd
Henri Remonen <henri@remonen.fi>
2023-10-26 09:22:23 +0300

    feat: show the Grafana link in admin panel

--- src/client/components/Admin/Analytics/RenderAnalytics.tsx
+++ src/client/components/Admin/Analytics/RenderAnalytics.tsx
@@ -1,26 +1,40 @@
 import React from 'react'
-import { Grid } from '@mui/material'
+import { useTranslation } from 'react-i18next'
+import { Alert, Box, Grid, Link } from '@mui/material'
 
 import FacultyAnalytics from './FacultyAnalytics'
 import SurveyAnalytics from './SurveyAnalytics'
 import UserCountAnalytics from './UserCountAnalytics'
 
-const RenderAnalytics = () => (
-  <Grid container spacing={2}>
-    <Grid item xs={12} xl={8}>
-      <FacultyAnalytics />
-    </Grid>
-    <Grid item xs={12} xl={8}>
+const RenderAnalytics = () => {
+  const { t } = useTranslation()
+
+  return (
+    <>
+      <Box sx={{ mt: 2 }}>
+        <Alert severity='info'>
+          <Link target='_blank' rel='noopener' href={t('admin:grafanaExtLink')}>
+            Grafana
+          </Link>
+        </Alert>
+      </Box>
       <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <SurveyAnalytics />
+        <Grid item xs={12} xl={8}>
+          <FacultyAnalytics />
         </Grid>
-        <Grid item xs={6}>
-          <UserCountAnalytics />
+        <Grid item xs={12} xl={8}>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <SurveyAnalytics />
+            </Grid>
+            <Grid item xs={6}>
+              <UserCountAnalytics />
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
-  </Grid>
-)
+    </>
+  )
+}
 
 export default RenderAnalytics
