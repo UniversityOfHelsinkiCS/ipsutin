6b1dc186d147be3368525fe2456457c931ed5e50
Henri Remonen <henri@remonen.fi>
2023-11-02 09:47:18 +0200

    style: remove border from the entry viewer

--- src/client/components/Admin/Entries/Entry.tsx
+++ src/client/components/Admin/Entries/Entry.tsx
@@ -20,37 +20,31 @@ const Entry = () => {
   if (!entry || isLoading || !results || !resultsFetched) return null
 
   return (
-    <Box sx={cardStyles.outerBox}>
-      <Box sx={resultStyles.resultWrapper}>
-        <Box sx={{ m: 4 }}>
-          <Typography
-            data-cy='result-section-title'
-            variant='h5'
-            component='div'
-          >
-            {t('admin:entryInfoTitle')}
+    <Box sx={resultStyles.resultWrapper}>
+      <Box sx={{ m: 4 }}>
+        <Typography data-cy='result-section-title' variant='h5' component='div'>
+          {t('admin:entryInfoTitle')}
+        </Typography>
+        <Box sx={{ mx: 2 }}>
+          <Typography variant='body2' sx={cardStyles.question}>
+            {t('admin:entryViewSurvey')}:{' '}
+            {t(`surveyNames:${entry.Survey.name}`)}
           </Typography>
-          <Box sx={{ mx: 2 }}>
-            <Typography variant='body2' sx={cardStyles.question}>
-              {t('admin:entryViewSurvey')}:{' '}
-              {t(`surveyNames:${entry.Survey.name}`)}
-            </Typography>
-            <Typography variant='body2' sx={cardStyles.question}>
-              {t('admin:entryViewFaculty')}: {entry.data.faculty}
-            </Typography>
-            <Typography variant='body2' sx={cardStyles.question}>
-              {t('admin:entryViewCreated')}:{' '}
-              {new Date(entry.createdAt).toLocaleString()}
-            </Typography>
-            <Typography variant='body2' sx={cardStyles.question}>
-              {t('admin:entryViewUpdated')}:{' '}
-              {new Date(entry.updatedAt).toLocaleString()}
-            </Typography>
-          </Box>
+          <Typography variant='body2' sx={cardStyles.question}>
+            {t('admin:entryViewFaculty')}: {entry.data.faculty}
+          </Typography>
+          <Typography variant='body2' sx={cardStyles.question}>
+            {t('admin:entryViewCreated')}:{' '}
+            {new Date(entry.createdAt).toLocaleString()}
+          </Typography>
+          <Typography variant='body2' sx={cardStyles.question}>
+            {t('admin:entryViewUpdated')}:{' '}
+            {new Date(entry.updatedAt).toLocaleString()}
+          </Typography>
         </Box>
-
-        <RenderResults surveyName={entry.Survey.name} resultData={entry.data} />
       </Box>
+
+      <RenderResults surveyName={entry.Survey.name} resultData={entry.data} />
     </Box>
   )
 }
