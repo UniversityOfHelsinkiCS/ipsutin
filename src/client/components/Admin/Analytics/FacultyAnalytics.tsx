62dd83af5a42aac422eb1f297896b0c88af0a167
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-03-06 12:42:40 +0200

    fixing eslint warnings

--- src/client/components/Admin/Analytics/FacultyAnalytics.tsx
+++ src/client/components/Admin/Analytics/FacultyAnalytics.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useTranslation } from 'react-i18next'
 import { Box, Paper, Typography } from '@mui/material'
 // eslint-disable-next-line import/no-extraneous-dependencies
