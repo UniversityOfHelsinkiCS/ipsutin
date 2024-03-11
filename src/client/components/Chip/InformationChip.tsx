71eb1bbb657b96e90e1d893323819898653d3d60
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-03-11 13:25:13 +0200

    fixed eslint

--- src/client/components/Chip/InformationChip.tsx
+++ src/client/components/Chip/InformationChip.tsx
@@ -2,22 +2,27 @@ import { Chip } from '@mui/material'
 
 import colors from '../../util/colors'
 
+import ChipWindow from './ChipWindow'
+
 interface InformationChipProps {
   title: string
   label: string
 }
 
 const InformationChip: React.FC<InformationChipProps> = ({ title, label }) => (
-  <Chip
-    key={label}
-    label={title}
-    sx={{
-      backgroundColor: (colors as any)[label],
-      marginX: '0.3rem',
-      fontWeight: 'normal',
-      color: 'black',
-    }}
-  />
+  <>
+    <Chip
+      key={label}
+      label={title}
+      sx={{
+        backgroundColor: (colors as any)[label],
+        marginX: '0.3rem',
+        fontWeight: 'normal',
+        color: 'black',
+      }}
+    />
+    <ChipWindow />
+  </>
 )
 
 export default InformationChip
