30fc0769edf6c45460a99a421acd32f3fce52a39
Henri Remonen <henri@remonen.fi>
2024-04-05 08:49:48 +0300

    style: fixing responsiveness

--- src/client/pages/MainPage/MainPage.tsx
+++ src/client/pages/MainPage/MainPage.tsx
@@ -1,55 +1,46 @@
 import { useTranslation } from 'react-i18next'
-import { Box, Grid, Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 
 import HeroSection from './HeroSection'
 import ProductGrid from './ProductGrid'
 import ServiceLinks from './ServiceLinks'
 
+const SectionHeading = ({ children }) => (
+  <Typography
+    component='h2'
+    sx={{
+      mb: 2,
+      fontSize: '24pt',
+      fontWeight: 700,
+      textTransform: 'uppercase',
+      letterSpacing: '-0.1rem',
+    }}
+  >
+    {children}
+  </Typography>
+)
+
 const MainPage = () => {
   const { t } = useTranslation()
 
   return (
-    <Box>
+    <Grid container sx={{ mx: 'auto', maxWidth: '1560px' }}>
       <HeroSection />
-      <Grid container sx={{ mx: 'auto', maxWidth: '1560px' }}>
-        <Grid item sm={12} sx={{ px: 4, mt: 4, textAlign: 'center' }}>
-          <Typography
-            component='h2'
-            sx={{
-              mb: 2,
-              fontSize: '24pt',
-              fontWeight: 700,
-              textTransform: 'uppercase',
-              letterSpacing: '-0.1rem',
-            }}
-          >
-            {t('mainPage:products')}
-          </Typography>
-          <ProductGrid />
-        </Grid>
-        <Grid
-          item
-          sm={12}
-          md={12}
-          xl={12}
-          sx={{ mt: 4, mb: 12, textAlign: 'center' }}
-        >
-          <Typography
-            component='h2'
-            sx={{
-              mb: 2,
-              fontSize: '24pt',
-              fontWeight: 700,
-              textTransform: 'uppercase',
-              letterSpacing: '-0.1rem',
-            }}
-          >
-            {t('mainPage:services')}
-          </Typography>
-          <ServiceLinks />
-        </Grid>
+      <Grid item sm={12} sx={{ px: 4, mt: 4, textAlign: 'center' }}>
+        <SectionHeading>{t('mainPage:products')}</SectionHeading>
+        <ProductGrid />
       </Grid>
-    </Box>
+      <Grid
+        item
+        sm={12}
+        md={12}
+        xl={12}
+        sx={{ mt: 4, mb: 12, textAlign: 'center' }}
+      >
+        <SectionHeading>{t('mainPage:services')}</SectionHeading>
+        <ServiceLinks />
+      </Grid>
+    </Grid>
   )
 }
 export default MainPage
