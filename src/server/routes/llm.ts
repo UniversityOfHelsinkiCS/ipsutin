597697ee0943ca727ba3cfb2d0cd0c661714ebc4
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-04-18 12:44:25 +0300

    refactor

--- src/server/routes/llm.ts
+++ src/server/routes/llm.ts
@@ -1,62 +1,9 @@
 import express from 'express'
 
-import { getPromptById } from '../data/inventorPrompts'
-import { getCompletionStream } from '../services/llm'
+import { askCurreAndAddToMessages, createUserMessage } from '../services/llm'
 import { Message } from '../types'
 
 const llmRouter = express.Router()
-
-async function readStream(stream: ReadableStream<Uint8Array>): Promise<string> {
-  const reader = stream.getReader()
-  let content = ''
-
-  while (true) {
-    // eslint-disable-next-line no-await-in-loop
-    const { value, done } = await reader.read()
-
-    if (done) break
-
-    const text = new TextDecoder().decode(value)
-    content += text
-  }
-
-  return content
-}
-
-async function askCurre(allMessages: Message[]): Promise<Message> {
-  const model = 'gpt-3.5-turbo'
-  const { stream } = await getCompletionStream(allMessages, model)
-  const content = await readStream(stream)
-
-  const assistantMessage: Message = {
-    role: 'assistant',
-    content,
-  }
-
-  return assistantMessage
-}
-
-function createUserMessage(input: string, promptId: number): Message {
-  const enhancementPrompt: string = getPromptById(promptId)
-  const fullPrompt: string = `${enhancementPrompt} ${input}`
-
-  const message: Message = {
-    role: 'user',
-    content: fullPrompt,
-  }
-
-  return message
-}
-
-async function askCurreAndAddToMessages(
-  message: Message,
-  messages: Message[]
-): Promise<Message> {
-  messages.push(message)
-  const curreResponse = await askCurre(messages)
-  messages.push(curreResponse)
-  return curreResponse
-}
 
 llmRouter.post('/step1', async (req, res) => {
   const { inventiveMessage, industrialMessage, publicMessage } = req.body
@@ -80,25 +27,27 @@ llmRouter.post('/step4', async (req, res) => {
 
   const messages: Message[] = []
 
-  // Step 1: Ask for idea refinement
+  // Step 2: Ask for idea refinement
   const ideaRefinementMessage = createUserMessage(ideaRefinement, 2)
   const ideaRefinementResponse = await askCurreAndAddToMessages(
     ideaRefinementMessage,
     messages
   )
 
-  // Step 2: Ask for claims refinement
+  // Step 3: Ask for claims refinement
   const industrialClaimsMessage = createUserMessage(
-    `${industrialRefinement} Claims: ${claims}`,
+    `${ideaRefinementResponse.content} Industrial applicability: ${industrialRefinement}`,
     3
   )
-  const claimsRefinementResponse = await askCurreAndAddToMessages(
+
+  const industrialRefinementResponse = await askCurreAndAddToMessages(
     industrialClaimsMessage,
     messages
   )
-  // Step 3: Final prompt
-  const finalPrompt = `${ideaRefinementResponse.content} Industry applicability: ${industrialRefinement.content} Claims: ${claimsRefinementResponse.content}`
+  // Step 4: Final prompt
+  const finalPrompt = `${ideaRefinementResponse.content} \nINDUSTRY APPLICABILITY: ${industrialRefinementResponse.content}\nCLAIMS: ${claims}`
   const finalMessage = createUserMessage(finalPrompt, 4)
+
   const finalResponse = await askCurreAndAddToMessages(finalMessage, messages)
 
   const finalResponseMessage: string = finalResponse.content
--- src/server/services/llm.ts
+++ src/server/services/llm.ts
@@ -1,51 +1,58 @@
 import { CURRE_URL } from '../../config'
+import { getPromptById } from '../data/inventorPrompts'
 import { Message } from '../types'
 
-export const getCompletionStream = async (
+export const getCompletion = async (
   allMessages: Message[],
   model: string
-) => {
+): Promise<string> => {
   const body = {
     options: {
       messages: [...allMessages],
       model,
     },
   }
-
   const response = await fetch(`${CURRE_URL}/api/ai/stream/innotin`, {
     method: 'POST',
     body: JSON.stringify(body),
     headers: { 'Content-Type': 'application/json' },
   })
 
-  const stream = response.body as unknown as ReadableStream
-
-  return { stream }
+  const content = await response.text()
+  return content
 }
 
-export const FirstStepMessageSend = async (
-  ideaTopic: string,
-  ideaIndustry: string,
-  ideaOrigin: string,
-  model: string
-) => {
-  const body = {
-    options: {
-      ideaTopic,
-      ideaIndustry,
-      ideaOrigin,
-      model,
-    },
+export async function askCurre(allMessages: Message[]): Promise<Message> {
+  const model = 'gpt-3.5-turbo'
+  const content = await getCompletion(allMessages, model) // Get content directly
+  const assistantMessage: Message = {
+    role: 'assistant',
+    content,
   }
-  const response = await fetch(
-    `http://${CURRE_URL}/api/ai/stream/inventorstep1`,
-    {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers: { 'Content-Type': 'application/json' },
-    }
-  )
 
-  const stream = response.body as unknown as ReadableStream
-  return { stream }
+  return assistantMessage
 }
+
+export async function askCurreAndAddToMessages(
+  message: Message,
+  messages: Message[]
+): Promise<Message> {
+  messages.push(message)
+  const curreResponse = await askCurre(messages)
+  messages.push(curreResponse)
+  return curreResponse
+}
+
+export function createUserMessage(input: string, promptId: number): Message {
+  const enhancementPrompt: string = getPromptById(promptId)
+  const fullPrompt: string = `${enhancementPrompt} ${input}`
+
+  const message: Message = {
+    role: 'user',
+    content: fullPrompt,
+  }
+
+  return message
+}
+
+export default getCompletion
