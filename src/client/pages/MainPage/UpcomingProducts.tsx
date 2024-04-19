e5b4f6cb405c3410b1727ed26f34c51838d3f478
Henri Remonen <henri@remonen.fi>
2024-04-19 10:56:34 +0300

    refactor: remove unused import

--- src/client/pages/MainPage/UpcomingProducts.tsx
+++ src/client/pages/MainPage/UpcomingProducts.tsx
@@ -1,4 +1,3 @@
-import { useTranslation } from 'react-i18next'
 import { Grid, Typography } from '@mui/material'
 
 const upcomingProducts = [
@@ -16,42 +15,25 @@ const upcomingProducts = [
   },
 ]
 
-const UpcomingProducts = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { t } = useTranslation()
-
-  return (
-    <Grid
-      container
-      spacing={4}
-      sx={{
-        maxWidth: '1024px',
-        mx: 'auto',
-        my: '8rem',
-        alignItems: 'center',
-      }}
-    >
-      {upcomingProducts.map((product) => (
-        <Grid item xs={12} md={3} key={product.title}>
-          <img
-            src={product.image}
-            alt=''
-            width='100px'
-            style={{ borderRadius: '50%', marginBottom: '1rem' }}
-          />
-          <Typography
-            component='p'
-            sx={{
-              textTransform: 'uppercase',
-              fontWeight: 'bold',
-              fontSize: { xs: '18pt', sm: '20pt', md: '24pt' },
-            }}
-          >
-            {product.title}
-          </Typography>
-        </Grid>
-      ))}
-      <Grid item xs={12} md={3}>
+const UpcomingProducts = () => (
+  <Grid
+    container
+    spacing={4}
+    sx={{
+      maxWidth: '1024px',
+      mx: 'auto',
+      my: '8rem',
+      alignItems: 'center',
+    }}
+  >
+    {upcomingProducts.map((product) => (
+      <Grid item xs={12} md={3} key={product.title}>
+        <img
+          src={product.image}
+          alt=''
+          width='100px'
+          style={{ borderRadius: '50%', marginBottom: '1rem' }}
+        />
         <Typography
           component='p'
           sx={{
@@ -60,11 +42,23 @@ const UpcomingProducts = () => {
             fontSize: { xs: '18pt', sm: '20pt', md: '24pt' },
           }}
         >
-          and more...
+          {product.title}
         </Typography>
       </Grid>
+    ))}
+    <Grid item xs={12} md={3}>
+      <Typography
+        component='p'
+        sx={{
+          textTransform: 'uppercase',
+          fontWeight: 'bold',
+          fontSize: { xs: '18pt', sm: '20pt', md: '24pt' },
+        }}
+      >
+        and more...
+      </Typography>
     </Grid>
-  )
-}
+  </Grid>
+)
 
 export default UpcomingProducts
