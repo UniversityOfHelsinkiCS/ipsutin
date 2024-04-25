74253027ae4de660869c12cb73d8f68e796432a5
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-04-25 11:09:43 +0300

    added original idea, refined idea, industrial applicability, and claims to final step

--- src/client/pages/InventorsAssistant/FinalStep.tsx
+++ src/client/pages/InventorsAssistant/FinalStep.tsx
@@ -5,10 +5,20 @@ import Markdown from '../../components/Common/Markdown'
 import SectionHeading from '../../components/Common/SectionHeading'
 
 type FinalStepProps = {
-  aiResponse2: string
+  aiResponse: string
+  originalIdea: string
+  ideaRefinement: string
+  industrialApplicability: string
+  claims: string
 }
 
-const FinalStep = ({ aiResponse2 }: FinalStepProps) => (
+const FinalStep = ({
+  aiResponse,
+  originalIdea,
+  ideaRefinement,
+  industrialApplicability,
+  claims,
+}: FinalStepProps) => (
   <>
     <SectionHeading level='h2' sx={{ mt: 8 }}>
       {t('inventorsAssistant:finalStepHeader1')}
@@ -17,12 +27,50 @@ const FinalStep = ({ aiResponse2 }: FinalStepProps) => (
     <Typography variant='body1' sx={{ mt: 2 }}>
       {t('inventorsAssistant:finalStepText1')}
     </Typography>
+
+    <Typography variant='body1' sx={{ mt: 2 }}>
+      {t('inventorsAssistant:finalStepOriginalIdea')}
+    </Typography>
+
+    <Alert severity='success' sx={{ my: 4, p: 4 }}>
+      <Typography variant='body1' sx={{ mt: 2 }}>
+        {originalIdea}
+      </Typography>
+    </Alert>
+
+    <Typography variant='body1' sx={{ mt: 2 }}>
+      {t('inventorsAssistant:finalStepRefinedIdea')}
+    </Typography>
+    <Alert severity='success' sx={{ my: 4, p: 4 }}>
+      <Typography variant='body1' sx={{ mt: 2 }}>
+        {ideaRefinement}
+      </Typography>
+    </Alert>
+
+    <Typography variant='body1' sx={{ mt: 2 }}>
+      {t('inventorsAssistant:finalStepIndustrialApplicability')}
+    </Typography>
+    <Alert severity='success' sx={{ my: 4, p: 4 }}>
+      <Typography variant='body1' sx={{ mt: 2 }}>
+        {industrialApplicability}
+      </Typography>
+    </Alert>
+
+    <Typography variant='body1' sx={{ mt: 2 }}>
+      {t('inventorsAssistant:finalStepClaims')}
+    </Typography>
+    <Alert severity='success' sx={{ my: 4, p: 4 }}>
+      <Typography variant='body1' sx={{ mt: 2 }}>
+        {claims}
+      </Typography>
+    </Alert>
+
     <Typography variant='body1' sx={{ mt: 2 }}>
       {t('inventorsAssistant:finalStepSummary')}
     </Typography>
-    <Alert severity={aiResponse2 ? 'success' : 'info'} sx={{ my: 4, p: 4 }}>
+    <Alert severity={aiResponse ? 'success' : 'info'} sx={{ my: 4, p: 4 }}>
       <Markdown>
-        {aiResponse2 || 'Grunching response for you, please wait...'}
+        {aiResponse || 'Grunching response for you, please wait...'}
       </Markdown>
     </Alert>
   </>
