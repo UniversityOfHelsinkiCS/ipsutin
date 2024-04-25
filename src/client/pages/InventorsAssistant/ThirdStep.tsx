409adb0c753c8be85165fb0cf6209824851eaa7b
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-04-25 11:07:48 +0300

    added aiResponse to thirdstep

--- src/client/pages/InventorsAssistant/ThirdStep.tsx
+++ src/client/pages/InventorsAssistant/ThirdStep.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import { Box, TextField, Typography } from '@mui/material'
+import { Alert, Box, TextField, Typography } from '@mui/material'
 import { t } from 'i18next'
 
+import Markdown from '../../components/Common/Markdown'
 import SectionHeading from '../../components/Common/SectionHeading'
 
 type ThirdStepProps = {
   refinementMessage: string
   setRefinementMessage: React.Dispatch<React.SetStateAction<string>>
+  aiResponse: string
 }
 
 const ThirdStep: React.FC<ThirdStepProps> = ({
   refinementMessage,
   setRefinementMessage,
+  aiResponse,
 }) => (
   <>
     <SectionHeading level='h2' sx={{ mt: 8 }}>
@@ -22,18 +25,27 @@ const ThirdStep: React.FC<ThirdStepProps> = ({
     </Typography>
 
     <Box component='section' sx={{ mt: 4 }}>
-      <Typography variant='body1' sx={{ my: 2 }}>
-        {t('inventorsAssistant:step3Header2')}
-      </Typography>
+      <Alert severity={aiResponse ? 'success' : 'info'} sx={{ my: 4, p: 4 }}>
+        <Markdown>
+          {aiResponse || 'Grunching response for you, please wait...'}
+        </Markdown>
+      </Alert>
 
-      <TextField
-        fullWidth
-        multiline
-        minRows={5}
-        value={refinementMessage}
-        onChange={(e) => setRefinementMessage(e.target.value)}
-        placeholder={t('inventorsAssistant:chatBoxPlaceholder')}
-      />
+      {aiResponse.length > 0 && (
+        <>
+          <Typography variant='body1' sx={{ my: 2 }}>
+            {t('inventorsAssistant:step3Header2')}
+          </Typography>
+          <TextField
+            fullWidth
+            multiline
+            minRows={5}
+            value={refinementMessage}
+            onChange={(e) => setRefinementMessage(e.target.value)}
+            placeholder={t('inventorsAssistant:chatBoxPlaceholder')}
+          />
+        </>
+      )}
     </Box>
   </>
 )
