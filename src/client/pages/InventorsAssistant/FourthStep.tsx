25e4c4c3b460185b60bad4978d0d7130972c7857
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-04-25 11:08:31 +0300

    added aiResponse to fourth step

--- src/client/pages/InventorsAssistant/FourthStep.tsx
+++ src/client/pages/InventorsAssistant/FourthStep.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import { List, ListItemText, TextField, Typography } from '@mui/material'
+import { Alert, List, ListItemText, TextField, Typography } from '@mui/material'
 import { t } from 'i18next'
 
+import Markdown from '../../components/Common/Markdown'
 import SectionHeading from '../../components/Common/SectionHeading'
 
 type FourthStepProps = {
   refinementMessage: string
   setRefinementMessage: React.Dispatch<React.SetStateAction<string>>
+  aiResponse: string
 }
 
 const FourthStep = ({
   refinementMessage,
   setRefinementMessage,
+  aiResponse,
 }: FourthStepProps) => (
   <>
     <SectionHeading level='h2' sx={{ mt: 8 }}>
@@ -38,6 +41,12 @@ const FourthStep = ({
     <Typography variant='body1' sx={{ mt: 2 }}>
       {t('inventorsAssistant:step4text3')}
     </Typography>
+
+    <Alert severity={aiResponse ? 'success' : 'info'} sx={{ my: 4, p: 4 }}>
+      <Markdown>
+        {aiResponse || 'Grunching response for you, please wait...'}
+      </Markdown>
+    </Alert>
 
     <Typography variant='body1' sx={{ my: 2 }}>
       {t('inventorsAssistant:step4text4')}
