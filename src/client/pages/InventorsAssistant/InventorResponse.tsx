2823cf6917c5aebe1ed4489a1e79657eb9f17d2d
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-05-02 09:21:54 +0300

    InventorsResponse Textfield fixed and green border added to mark it as AI-output

--- src/client/pages/InventorsAssistant/InventorResponse.tsx
+++ src/client/pages/InventorsAssistant/InventorResponse.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Alert, Box, Button, TextField } from '@mui/material'
 
 import Markdown from '../../components/Common/Markdown'
@@ -10,6 +10,10 @@ type InventorResponseProps = {
 const InventorResponse = ({ aiResponse }: InventorResponseProps) => {
   const [editMode, setEditMode] = useState(false)
   const [editedResponse, setEditedResponse] = useState(aiResponse)
+
+  useEffect(() => {
+    setEditedResponse(aiResponse)
+  }, [aiResponse])
 
   const toggleEditMode = () => {
     setEditMode(!editMode)
@@ -33,6 +37,7 @@ const InventorResponse = ({ aiResponse }: InventorResponseProps) => {
             minRows={5}
             value={editedResponse}
             onChange={handleResponseChange}
+            sx={{ border: '4px solid green' }}
           />
           <Button
             onClick={handleSave}
@@ -54,14 +59,17 @@ const InventorResponse = ({ aiResponse }: InventorResponseProps) => {
         <Markdown>
           {aiResponse || 'Grunching response for you, please wait...'}
         </Markdown>
-        <Button
-          onClick={toggleEditMode}
-          variant='outlined'
-          color='primary'
-          sx={{ mt: 2 }}
-        >
-          Edit
-        </Button>
+
+        {aiResponse && aiResponse.length > 0 && (
+          <Button
+            onClick={toggleEditMode}
+            variant='outlined'
+            color='primary'
+            sx={{ mt: 2 }}
+          >
+            Edit
+          </Button>
+        )}
       </>
     </Alert>
   )
