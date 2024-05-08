575fca3e0e647f8de2414204233a729da6ea1cae
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-05-08 11:16:36 +0300

    added timer for successful alert - 6 seconds

--- src/client/pages/InventorsAssistant/LlmInputFeedback.tsx
+++ src/client/pages/InventorsAssistant/LlmInputFeedback.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { Alert, Box } from '@mui/material'
 
 import Markdown from '../../components/Common/Markdown'
@@ -11,16 +12,32 @@ const LlmInputFeedback = ({
   aiInputFeedback,
   alertSeverity,
 }: LlmInputFeedbackProps) => {
+  const [visible, setVisible] = useState<boolean>(true)
+
   let message = aiInputFeedback || 'Checking the quality of your input...'
 
   if (alertSeverity === 'info') {
     message = 'Checking the quality of your input...'
   }
+
+  useEffect(() => {
+    if (alertSeverity === 'success') {
+      const timer = setTimeout(() => {
+        setVisible(false)
+      }, 6000)
+      return () => clearTimeout(timer)
+    }
+
+    return undefined
+  }, [alertSeverity])
+
   return (
     <Box sx={{ mt: 4 }}>
-      <Alert severity={alertSeverity} sx={{ my: 4, p: 4, width: '100%' }}>
-        <Markdown>{message}</Markdown>
-      </Alert>
+      {visible && (
+        <Alert severity={alertSeverity} sx={{ my: 4, p: 4, width: '100%' }}>
+          <Markdown>{message}</Markdown>
+        </Alert>
+      )}
     </Box>
   )
 }
