213c4d5e56a654f7c5444ef7ee5e61fae9680144
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-05-10 10:47:09 +0300

    submit again button visible only after unsuccesful input

--- src/client/pages/InventorsAssistant/UserInput.tsx
+++ src/client/pages/InventorsAssistant/UserInput.tsx
@@ -21,6 +21,7 @@ const UserInput: React.FC<UserInputProps> = ({
 }) => {
   const [inputStep, setInputStep] = useState(false)
   const [buttonText, setButtonText] = useState('Next step')
+
   return (
     <Box component='section' sx={{ mt: 4 }}>
       <TextField
@@ -31,27 +32,30 @@ const UserInput: React.FC<UserInputProps> = ({
         onChange={(e) => setInventiveMessage(e.target.value)}
         placeholder={t('inventorsAssistant:chatBoxPlaceholder')}
       />
+      {}
 
-      <Button
-        sx={{
-          mx: 'auto',
-          px: 12,
-          my: 4,
-          borderRadius: '1rem',
-          textTransform: 'capitalize',
-          fontWeight: '600',
-          fontSize: '12pt',
-        }}
-        variant='contained'
-        color='secondary'
-        onClick={() => {
-          handleStepCheck()
-          setInputStep(true)
-          setButtonText('Submit again')
-        }}
-      >
-        {buttonText}
-      </Button>
+      {(!inputStep || (inputStep && aiInputFeedbackSuccess === 'warning')) && (
+        <Button
+          sx={{
+            mx: 'auto',
+            px: 12,
+            my: 4,
+            borderRadius: '1rem',
+            textTransform: 'capitalize',
+            fontWeight: '600',
+            fontSize: '12pt',
+          }}
+          variant='contained'
+          color='secondary'
+          onClick={() => {
+            handleStepCheck()
+            setInputStep(true)
+            setButtonText('Submit again')
+          }}
+        >
+          {buttonText}
+        </Button>
+      )}
 
       {inputStep && (
         <LlmInputFeedback
