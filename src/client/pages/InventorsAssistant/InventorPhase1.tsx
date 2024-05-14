3eda983db7f6befdcc346c4d1584b6f96a379069
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-05-14 14:04:01 +0300

    integrated inventorStepper into phase1

--- src/client/pages/InventorsAssistant/InventorPhase1.tsx
+++ src/client/pages/InventorsAssistant/InventorPhase1.tsx
@@ -4,6 +4,7 @@ import { Box, Button } from '@mui/material'
 import apiClient from '../../util/apiClient'
 
 import FirstStep from './FirstStep'
+import InventorsIllustration from './InventorIllustration'
 import { useInventorsContext } from './InventorsContext'
 import InventorStepper from './InventorStepper'
 import StepZero from './StepZero'
@@ -37,56 +38,56 @@ const InventorPhase1 = () => {
 
   return (
     <Box component='article'>
+      <InventorsIllustration />
+      {currentStep > 0 && <InventorStepper />}
+
       <StepZero currentStep={currentStep} setCurrentStep={setCurrentStep} />
 
       <Box component='section' sx={{ mx: 'auto', maxWidth: '1024px' }}>
         {currentStep > 0 && (
-          <>
-            <FirstStep
-              currentStep={currentStep}
-              setCurrentStep={setCurrentStep}
-              inventiveMessage={inventiveMessage}
-              setInventiveMessage={setInventiveMessage}
-              publicityMessage={publicityMessage}
-              setPublicityMessage={setPublicityMessage}
-              industrialMessage={industrialMessage}
-              setIndustrialMessage={setIndustrialMessage}
-            />
-            <InventorStepper />
-            {currentStep === 4 && (
-              <Box
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                <Button
-                  sx={{
-                    mx: 'auto',
-                    px: 12,
-                    my: 4,
-                    borderRadius: '1rem',
-                    textTransform: 'capitalize',
-                    fontWeight: '600',
-                    fontSize: '12pt',
-                  }}
-                  variant='contained'
-                  color='secondary'
-                  onClick={() => {
-                    handleFirstStep()
-                    setCurrentStep(5)
-                    navigate({
-                      pathname: './phase2',
-                      search: location.search,
-                    })
-                  }}
-                >
-                  Next step
-                </Button>
-              </Box>
-            )}
-          </>
+          <FirstStep
+            currentStep={currentStep}
+            setCurrentStep={setCurrentStep}
+            inventiveMessage={inventiveMessage}
+            setInventiveMessage={setInventiveMessage}
+            publicityMessage={publicityMessage}
+            setPublicityMessage={setPublicityMessage}
+            industrialMessage={industrialMessage}
+            setIndustrialMessage={setIndustrialMessage}
+          />
+        )}
+        {currentStep === 4 && (
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Button
+              sx={{
+                mx: 'auto',
+                px: 12,
+                my: 4,
+                borderRadius: '1rem',
+                textTransform: 'capitalize',
+                fontWeight: '600',
+                fontSize: '12pt',
+              }}
+              variant='contained'
+              color='secondary'
+              onClick={() => {
+                handleFirstStep()
+                setCurrentStep(5)
+                navigate({
+                  pathname: './phase2',
+                  search: location.search,
+                })
+              }}
+            >
+              Next step
+            </Button>
+          </Box>
         )}
       </Box>
     </Box>
