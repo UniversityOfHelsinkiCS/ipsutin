db86bcf4336564fb305cf4eab44199b1262423ca
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-05-22 13:13:12 +0300

    removed legacy code: curre api call

--- src/server/services/llm.ts
+++ src/server/services/llm.ts
@@ -6,7 +6,7 @@ import {
   OpenAIClient,
 } from '@azure/openai'
 
-import { CURRE_URL, validModels } from '../../config'
+import { validModels } from '../../config'
 import { getPromptById } from '../data/inventorPrompts'
 import {
   APIError,
@@ -15,37 +15,12 @@ import {
   Message,
   ValidatedInput,
 } from '../types'
-import {
-  AZURE_API_KEY,
-  AZURE_RESOURCE,
-  CURRE_API_PASSWORD,
-} from '../util/config'
+import { AZURE_API_KEY, AZURE_RESOURCE } from '../util/config'
 import logger from '../util/logger'
 
 const endpoint = `https://${AZURE_RESOURCE}.openai.azure.com/`
 
 const client = new OpenAIClient(endpoint, new AzureKeyCredential(AZURE_API_KEY))
-
-export const getCompletion = async (
-  allMessages: Message[],
-  model: string
-): Promise<string> => {
-  const body = {
-    options: {
-      messages: [...allMessages],
-      model,
-      CURRE_API_PASSWORD_INNOTIN: CURRE_API_PASSWORD,
-    },
-  }
-  const response = await fetch(`${CURRE_URL}/api/ai/stream/innotin`, {
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' },
-  })
-  console.log('RESPONSE: ', response)
-  const content = await response.text()
-  return content
-}
 
 const getMockCompletionEvents: () => Promise<
   EventStream<ChatCompletions>
@@ -118,16 +93,6 @@ export async function askCurreAndAddToMessages(
   messages.push(curreResponse)
   return curreResponse
 }
-export async function askCurre(allMessages: Message[]): Promise<Message> {
-  const model = 'gpt-3.5-turbo'
-  const content = await getCompletion(allMessages, model) // Get content directly
-  const assistantMessage: Message = {
-    role: 'assistant',
-    content,
-  }
-
-  return assistantMessage
-}
 
 export function createUserMessage(input: string, promptId: number): Message {
   const enhancementPrompt: string = getPromptById(promptId)
@@ -174,5 +139,3 @@ export function handleValidationResponse(content: string): ValidatedInput {
     return { success: false, feedback: content, error: true }
   }
 }
-
-export default getCompletion
