92a03a4dbf1e396a551c17e24ee19bca6bbb1003
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-05-27 15:04:14 +0300

    refactored summary of Invention Report to own componenet which will also be the one shared by email instead of whole finalStep

---
+++ src/client/pages/InventorsAssistant/InventionReport.tsx
@@ -0,0 +1,32 @@
+import { Dispatch, SetStateAction } from 'react'
+import { Box } from '@mui/material'
+import { t } from 'i18next'
+
+import SectionHeading from '../../components/Common/SectionHeading'
+import useResultRefCallback from '../../hooks/useResultRefCallback'
+
+import LlmResponse from './LlmResponse'
+
+type InventionReportProps = {
+  aiResponse: string
+  setAiResponse: Dispatch<SetStateAction<string>>
+}
+
+const InventionReport = ({
+  aiResponse,
+  setAiResponse,
+}: InventionReportProps) => {
+  const refCallback = useResultRefCallback()
+
+  return (
+    <Box ref={aiResponse && aiResponse.length > 0 ? refCallback : null}>
+      <SectionHeading level='h3' sx={{ mt: 8 }}>
+        {t('inventorsAssistant:finalStepSummary')}
+      </SectionHeading>
+
+      <LlmResponse aiResponse={aiResponse} setEditedResponse={setAiResponse} />
+    </Box>
+  )
+}
+
+export default InventionReport
