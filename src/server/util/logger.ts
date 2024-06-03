3c9a4a4758bb6856677c9a12a0732835e73751a4
Henri Remonen <henri@remonen.fi>
2024-06-03 11:59:38 +0300

    chore: fix winston import

--- src/server/util/logger.ts
+++ src/server/util/logger.ts
@@ -1,6 +1,6 @@
 import os from 'os'
 import winston from 'winston'
-import WinstonGelfTransporter from 'winston-gelf-transporter'
+import { WinstonGelfTransporter } from 'winston-gelf-transporter'
 
 import { inProduction } from '../../config'
 
