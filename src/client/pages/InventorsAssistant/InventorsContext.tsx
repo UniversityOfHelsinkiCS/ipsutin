a7e8fc34ab0a2350cc4baf5eb777b6260648063f
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-08-06 11:01:47 +0300

    removed unneeded types

--- src/client/pages/InventorsAssistant/InventorsContext.tsx
+++ src/client/pages/InventorsAssistant/InventorsContext.tsx
@@ -18,12 +18,8 @@ interface InventorsContextValue {
   setAiResponse1Ready: React.Dispatch<React.SetStateAction<boolean>>
   aiResponse2: string
   setAiResponse2: React.Dispatch<React.SetStateAction<string>>
-  aiResponse2Ready: boolean
-  setAiResponse2Ready: React.Dispatch<React.SetStateAction<boolean>>
   aiResponse3: string
   setAiResponse3: React.Dispatch<React.SetStateAction<string>>
-  aiResponse3Ready: boolean
-  setAiResponse3Ready: React.Dispatch<React.SetStateAction<boolean>>
   aiResponse4: string
   setAiResponse4: React.Dispatch<React.SetStateAction<string>>
   aiResponse4Ready: boolean
