12b2269257f20bc9e4608abc6c29892bdf1c4129
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-08-16 15:42:50 +0300

    ErrorAlert component to communicate when an error has occured and allows user to Try Again

---
+++ src/client/pages/InventorsAssistant/ErrorAlert.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { Alert, Box, Button, Typography } from '@mui/material'
+import { t } from 'i18next'
+
+import Markdown from '../../components/Common/Markdown'
+
+interface ErrorAlertProps {
+  error: string
+  handleTryAgain: () => void
+}
+
+const ErrorAlert: React.FC<ErrorAlertProps> = ({ error, handleTryAgain }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  return (
+    <Box sx={{ mt: 4 }}>
+      <Alert severity='warning' sx={{ my: 4, p: 4, width: '100%' }}>
+        <Markdown>## An Error Occured</Markdown>
+
+        <Typography
+          variant='body2'
+          onClick={() => setShowDetails(!showDetails)}
+          sx={{
+            cursor: 'pointer',
+            color: 'text.secondary',
+            mt: 2,
+            mb: showDetails ? 2 : 0,
+            textDecoration: showDetails ? 'underline' : 'none',
+          }}
+        >
+          {showDetails ? 'Hide Details' : 'Show Details'}
+        </Typography>
+
+        {showDetails && <Markdown>{error}</Markdown>}
+
+        <Button
+          sx={{
+            mt: 'auto',
+            alignSelf: 'flex-end',
+            px: 3,
+            my: 3,
+            borderRadius: '1rem',
+            textTransform: 'capitalize',
+            fontWeight: '500',
+            fontSize: '11pt',
+          }}
+          variant='contained'
+          color='secondary'
+          onClick={() => handleTryAgain()}
+        >
+          {t('inventorsAssistant:tryAgain')}
+        </Button>
+      </Alert>
+    </Box>
+  )
+}
+
+export default ErrorAlert
