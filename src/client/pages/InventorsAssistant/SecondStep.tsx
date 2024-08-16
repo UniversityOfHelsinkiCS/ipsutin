c044a22b0178c36994aefea62d6bea88935418c5
AlexanderEngelhardt <ale.engelhardt@outlook.com>
2024-08-16 17:09:47 +0300

    ErrorAlert rendered conditionally when Error Occures

--- src/client/pages/InventorsAssistant/SecondStep.tsx
+++ src/client/pages/InventorsAssistant/SecondStep.tsx
@@ -3,8 +3,12 @@ import { Box, Typography } from '@mui/material'
 import { t } from 'i18next'
 
 import SectionHeading from '../../components/Common/SectionHeading'
+import { fetchStream } from '../../util/apiClient'
 
+import ErrorAlert from './ErrorAlert'
+import { useInventorsContext } from './InventorsContext'
 import LlmResponse from './LlmResponse'
+import processStream from './StreamReader'
 
 type SecondStepProps = {
   current: boolean
@@ -20,28 +24,66 @@ const SecondStep = ({
   aiResponse,
   aiResponseReady,
   setEditModeGlobal,
-}: SecondStepProps) => (
-  <>
-    <SectionHeading level='h2' sx={{ mt: 8 }}>
-      {t('inventorsAssistant:step2Header1')}
-    </SectionHeading>
-    <Box component='section' sx={{ mt: 4 }}>
-      <Typography variant='body1' sx={{ mt: 2 }}>
-        {t('inventorsAssistant:step2text1')}
-      </Typography>
-      <Typography variant='body1' sx={{ mt: 2 }}>
-        {t('inventorsAssistant:step2text2')}
-      </Typography>
-    </Box>
+}: SecondStepProps) => {
+  const {
+    inventiveMessage,
+    publicityMessage,
+    industrialMessage,
+    setAiResponse1Ready,
+    aiResponse1Error,
+    setAiResponse1Error,
+  } = useInventorsContext()
 
-    <LlmResponse
-      current={current}
-      aiResponse={aiResponse}
-      aiResponseReady={aiResponseReady}
-      setEditedResponse={setAiResponse1}
-      setEditModeGlobal={setEditModeGlobal}
-    />
-  </>
-)
+  const handleFirstStep = async () => {
+    setAiResponse1Error(null)
+    setAiResponse1('')
+
+    const { stream, error } = await fetchStream('/llm/step1', {
+      inventiveMessage,
+      industrialMessage,
+      publicityMessage,
+    })
+
+    if (error) {
+      setAiResponse1Error(`An error occurred: ${error}`)
+      return
+    }
+
+    if (stream) {
+      await processStream(stream, setAiResponse1, setAiResponse1Ready)
+    } else {
+      setAiResponse1Error('An unknown error occurred.')
+    }
+  }
+  return (
+    <>
+      <SectionHeading level='h2' sx={{ mt: 8 }}>
+        {t('inventorsAssistant:step2Header1')}
+      </SectionHeading>
+      <Box component='section' sx={{ mt: 4 }}>
+        <Typography variant='body1' sx={{ mt: 2 }}>
+          {t('inventorsAssistant:step2text1')}
+        </Typography>
+        <Typography variant='body1' sx={{ mt: 2 }}>
+          {t('inventorsAssistant:step2text2')}
+        </Typography>
+      </Box>
+      {aiResponse1Error ? (
+        <ErrorAlert
+          error={aiResponse1Error}
+          handleTryAgain={() => handleFirstStep()}
+        />
+      ) : (
+        <LlmResponse
+          current={current}
+          aiResponse={aiResponse}
+          aiResponseReady={aiResponseReady}
+          setEditedResponse={setAiResponse1}
+          setEditModeGlobal={setEditModeGlobal}
+        />
+      )}
+    </>
+  )
+}
 
 export default SecondStep
